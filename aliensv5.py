24dc03019a01a0a8eb63cb2a55740ef557ebdc2b
FrancisRK <francistheboffin@gmail.com>
2018-04-02 18:08:31 +0100

    Removed older files, removed explosions from shots

--- aliensv5.py
+++ aliensv5.py
@@ -12,10 +12,10 @@ if not pygame.image.get_extended():
 
 
 #game constants
-MAX_SHOTS      = 2      #most player bullets onscreen
-ALIEN_ODDS     = 22     #chances a new alien appears
+MAX_SHOTS      = 2       #most player bullets onscreen
+ALIEN_ODDS     = 22      #chances a new alien appears
 BOMB_ODDS      = 500     #chances a new bomb will drop
-ALIEN_RELOAD   = 4     #frames between new aliens
+ALIEN_RELOAD   = 4       #frames between new aliens
 SCREENRECT     = Rect(0, 0, 640, 480)
 SCORE          = 0
 
@@ -25,35 +25,23 @@ main_dir = os.path.split(os.path.abspath(__file__))[0]
 
 # Define utility functions
 
-def load_image(file):
+def load_image(file, transparent):
     "loads an image, prepares it for play"
     file = os.path.join(main_dir, 'data', file)
     try:
         surface = pygame.image.load(file)
     except pygame.error:
         raise SystemExit('Could not load image "%s" %s'%(file, pygame.get_error()))
+    if transparent: # Sets areas of image which are transparent to be the same colour as the surface background
+        corner = surface.get_at((50, 0))
+        surface.set_colorkey(corner, RLEACCEL)
     return surface.convert()
 
 def load_images(*files):
     imgs = []
     for file in files:
-        imgs.append(load_image(file))
+        imgs.append(load_image(file, 0))
     return imgs
-
-
-class dummysound:
-    def play(self): pass
-
-def load_sound(file):
-    if not pygame.mixer: return dummysound()
-    file = os.path.join(main_dir, 'data', file)
-    try:
-        sound = pygame.mixer.Sound(file)
-        return sound
-    except pygame.error:
-        print ('Warning, unable to load, %s' % file)
-    return dummysound()
-
 
 
 # each type of game object gets an init and an
@@ -116,9 +104,9 @@ class Alien(pygame.sprite.Sprite):
         self.image = self.images[self.frame//self.animcycle%3]
 
 ################################################################################
-   
+
 class Bossleft(pygame.sprite.Sprite):
-    speed=0
+    speed = 0
     animcycle = 12
     images = []
     def __init__(self):
@@ -126,19 +114,33 @@ class Bossleft(pygame.sprite.Sprite):
         self.image = self.images[0]
         self.rect = self.image.get_rect()
         self.frame = 0
-        self.rect.center = SCREENRECT.midleft
+        self.rect.center = (270,200)
         self.speed = 0
 
     def update(self):
         self.rect.move_ip(Bossleft.speed,0)
-        if not SCREENRECT.contains(self.rect):
-            Bossleft.speed = -Bossleft.speed
-            #self.rect.top = self.rect.bottom + 1
-            self.rect = self.rect.clamp(SCREENRECT)
-        self.frame = self.frame + 1
-        self.image = self.images[self.frame//self.animcycle%3]
+        if not SCREENRECT.colliderect(self.rect):
+            self.kill()
 
-   
+
+class Bossright(pygame.sprite.Sprite):
+    speed = 0
+    animcycle = 12
+    images = []
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self, self.containers)
+        self.image = self.images[0]
+        self.rect = self.image.get_rect()
+        self.frame = 0
+        self.rect.center = (370,200)
+        self.speed = 0
+
+    def update(self):
+        self.rect.move_ip(Bossright.speed,0)
+        if not SCREENRECT.colliderect(self.rect):
+            self.kill()
+
+
 class Shield(pygame.sprite.Sprite):
     speed = 0
     animcycle = 12
@@ -147,16 +149,14 @@ class Shield(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self, self.containers)
         self.image = self.images[0]
         self.rect = self.image.get_rect()
-#        self.facing = 0
+        self.health = 9 + random.choice((0,5))
         self.frame = 0
-#        if self.facing == 0:
-        self.rect.center = (320,200)
+        self.rect.center = (320,260)
 
     def update(self):
         self.rect.move_ip(0, 0)
         self.frame = self.frame + 1
         self.image = self.images[self.frame//self.animcycle%3]
-
 
 
 ################################################################################
@@ -176,6 +176,20 @@ class Explosion(pygame.sprite.Sprite):
         self.image = self.images[self.life//self.animcycle%2]
         if self.life <= 0: self.kill()
 
+class Points(pygame.sprite.Sprite):
+    defaultlife = 12
+    animcycle = 3
+    images = []
+    def __init__(self, actor):
+        pygame.sprite.Sprite.__init__(self, self.containers)
+        self.image = self.images[0]
+        self.rect = self.image.get_rect(center=actor.rect.midbottom)
+        self.life = self.defaultlife
+
+    def update(self):
+        self.life = self.life - 1
+        self.image = self.images[self.life//self.animcycle%2]
+        if self.life <= 0: self.kill()
 
 class Shot(pygame.sprite.Sprite):
     speed = -11
@@ -224,59 +238,60 @@ class Score(pygame.sprite.Sprite):
             self.image = self.font.render(msg, 0, self.color)
 
 
+class Finalscore(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.font = pygame.font.Font(None, 50)
+        self.font.set_italic(1)
+        self.color = Color('white')
+        msg = "Score: %d" % SCORE
+        self.image = self.font.render(msg, 0, self.color)
+        self.rect = self.image.get_rect()
+        self.rect.center = SCREENRECT.center
 
 def main(winstyle = 0):
     # Initialize pygame
-    #pygame.init()
     pygame.display.init()
     pygame.font.init()
-
-    if pygame.mixer and not pygame.mixer.get_init():
-        print ('Warning, no sound')
-        pygame.mixer = None
 
     # Set the display mode
     winstyle = 0  # |FULLSCREEN
     bestdepth = pygame.display.mode_ok(SCREENRECT.size, winstyle, 32)
     screen = pygame.display.set_mode(SCREENRECT.size, winstyle, bestdepth)
 
-    #Load images, assign to sprite classes
-    #(do this before the classes are used, after screen setup)
-    img = load_image('player1.gif')
+    # Load images, assign to sprite classes
+    # (do this before the classes are used, after screen setup)
+    img = load_image('player1.gif', 0)
     Player.images = [img, pygame.transform.flip(img, 1, 0)]
-    img = load_image('explosion1.gif')
+    img = load_image('explosion1.gif', 0)
     Explosion.images = [img, pygame.transform.flip(img, 1, 1)]
+    Points.images = load_images('onepoint1.gif','onepoint2.gif','onepoint3.gif')
     Alien.images = load_images('alien1.gif', 'alien2.gif', 'alien3.gif')
-    Bossleft.images = load_images('bossleftsmall.png','bossleftsmall.png','bossleftsmall.png')
+    Bossleft.images = [load_image('bossleftsmallv2.png', 1)]
+    Bossright.images = [load_image('bossrightsmallv2.png', 1)]
     Shield.images = load_images('shield1.gif','shield2.gif','shield3.gif')
-    Bomb.images = [load_image('bomb.gif')]
-    Shot.images = [load_image('shot.gif')]
+    Bomb.images = [load_image('bomb.gif', 0)]
+    Shot.images = [load_image('shot.gif', 0)]
+    finalscreen = load_image('finalscreen.png', 0)
 
-    #decorate the game window
+    # Decorate the game window
     icon = pygame.transform.scale(Alien.images[0], (32, 32))
     pygame.display.set_icon(icon)
     pygame.display.set_caption('Pygame Aliens')
     pygame.mouse.set_visible(0)
 
-    #create the background, tile the bgd image
-    bgdtile = load_image('background.gif')
+    # Create the background, tile the bgd image
+    bgdtile = load_image('background.gif', 0)
     background = pygame.Surface(SCREENRECT.size)
     for x in range(0, SCREENRECT.width, bgdtile.get_width()):
         background.blit(bgdtile, (x, 0))
     screen.blit(background, (0,0))
     pygame.display.flip()
 
-    #load the sound effects
-    boom_sound = load_sound('boom.wav')
-    shoot_sound = load_sound('car_door.wav')
-    if pygame.mixer:
-        music = os.path.join(main_dir, 'data', 'house_lo.wav')
-        pygame.mixer.music.load(music)
-        pygame.mixer.music.play(-1)
-
     # Initialize Game Groups
     aliens = pygame.sprite.Group()
     bosslefts = pygame.sprite.GroupSingle()
+    bossrights = pygame.sprite.GroupSingle()
     shields = pygame.sprite.Group()
     shots = pygame.sprite.Group()
     bombs = pygame.sprite.Group()
@@ -287,10 +302,12 @@ def main(winstyle = 0):
     Player.containers = all
     Alien.containers = aliens, all, lastalien
     Bossleft.containers = bosslefts, all
+    Bossright.containers = bossrights, all
     Shield.containers = shields, all
     Shot.containers = shots, all
     Bomb.containers = bombs, all
     Explosion.containers = all
+    Points.containers = all
     Score.containers = all
 
     # Create Some Starting Values
@@ -304,33 +321,32 @@ def main(winstyle = 0):
     player = Player()
     Alien() #note, this 'lives' because it goes into a sprite group
     Bossleft()
-    Shield()
+    Bossright()
+    shield = Shield()
     if pygame.font:
         all.add(Score())
 
-
     while player.alive():
 
-        #get input
+        # Get input
         for event in pygame.event.get():
             if event.type == QUIT or \
                 (event.type == KEYDOWN and event.key == K_ESCAPE):
                     return
         keystate = pygame.key.get_pressed()
 
-        # clear/erase the last drawn sprites
+        # Clear/erase the last drawn sprites
         all.clear(screen, background)
 
-        #update all the sprites
+        # Update all the sprites
         all.update()
 
-        #handle player input
+        # Handle player input
         direction = keystate[K_RIGHT] - keystate[K_LEFT]
         player.move(direction)
         firing = keystate[K_SPACE]
         if not player.reloading and firing and len(shots) < MAX_SHOTS:
             Shot(player.gunpos())
-            shoot_sound.play()
         player.reloading = firing
 
         # Create new alien
@@ -340,38 +356,59 @@ def main(winstyle = 0):
             Alien()
             alienreload = ALIEN_RELOAD
 
-        if SCORE == 10:
-            Bossleft()
+#        if (SCORE/10).is_integer() and len(bosslefts) < 1:
+#            Bossleft()
+#            Bossright()
+#            shield = Shield()
 
         # Drop bombs
         for a in aliens:
             if a and not int(random.random() * BOMB_ODDS):
                 Bomb(a)
 
-        # Detect collisions
-        for alien in pygame.sprite.spritecollide(player, aliens, 1):
-            boom_sound.play()
-            Explosion(alien)
-            Explosion(player)
-            SCORE = SCORE + 1
-            player.kill()
-
+        # Collision detection for aliens and shots
         for alien in pygame.sprite.groupcollide(shots, aliens, 1, 1).keys():
-            boom_sound.play()
-            Explosion(alien)
+            Points(alien)
             SCORE = SCORE + 1
 
+        # Collision detection for bombs and player
         for bomb in pygame.sprite.spritecollide(player, bombs, 1):
-            boom_sound.play()
             Explosion(player)
             Explosion(bomb)
             player.kill()
 
-        pygame.sprite.groupcollide(shields, bombs, 0, 1)
+################################################################################
 
-        if len(pygame.sprite.groupcollide(shields, shots, 1, 1))>0:
-            Bossleft.speed=5
-        
+        # Collision detection for bombs
+        pygame.sprite.groupcollide(shields, bombs, 0, 1)
+        pygame.sprite.groupcollide(bosslefts, bombs, 0, 1)
+        pygame.sprite.groupcollide(bossrights, bombs, 0, 1)
+
+        # Collsion for shield and shots
+        if len(shields) > 0:
+            if shield.health > 0:
+                for shot in pygame.sprite.groupcollide(shots, shields, 1, 0).keys():
+                    shield.health = shield.health - 1
+                    Points(shot)
+
+        # Collision for shield and shots on final hit
+            else:
+                for shield in pygame.sprite.groupcollide(shields, shots, 0, 0).keys():
+                    for shot in pygame.sprite.groupcollide(shots, shields, 1, 1).keys():
+                        Points(shot)
+                        Bossleft.speed = -5
+                        Bossright.speed = 7
+
+        # Collision detection for boss and shots
+        if len(shields) == 0:
+            for bossleft in pygame.sprite.groupcollide(bosslefts, shots, 1, 1):
+                Explosion(bossleft)
+                SCORE = SCORE + 5
+            for bossright in pygame.sprite.groupcollide(bossrights, shots, 1, 1):
+                Explosion(bossright)
+                SCORE = SCORE + 10
+
+################################################################################
 
         #draw the scene
         dirty = all.draw(screen)
@@ -380,11 +417,18 @@ def main(winstyle = 0):
         #cap the framerate
         clock.tick(40)
 
-    if pygame.mixer:
-        pygame.mixer.music.fadeout(1000)
-    pygame.time.wait(1000)
-    pygame.quit()
+    pygame.time.wait(500)
 
+    # Final screen and score displaying
+    screen.blit(finalscreen, (0,0))
+    pygame.display.flip()
+    all.empty()
+    all.add(Finalscore())
+    dirty = all.draw(screen)
+    pygame.display.update(dirty)
+
+    pygame.time.wait(2000)
+    pygame.quit()
 
 
 #call the "main" function if running this script
